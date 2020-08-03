b572b5e0cb450727bafe53d8a0a7cb60744d2297
Pierrick Gouerec <pgouerec@adfinitas.fr>
2020-08-03 17:27:30 +0200

    [+] Adding html block comment - Updating navigation collapse aniomation

--- src/asset/js/navigation.js
+++ src/asset/js/navigation.js
@@ -22,6 +22,9 @@ function toggleNavbar() {
     $('.navigation__collapse').animate({
       left: +navWidth,
     }, 500);
+    $('.popup').animate({
+      right: 40,
+    }, 500);
     $('.toggler').removeClass('active');
     if ($(window).width() < 576)
       $('body').css('overflow-y', 'auto');
@@ -29,7 +32,11 @@ function toggleNavbar() {
     $('.navigation__collapse').animate({
       left: navWidth - collapseWidth,
     }, 500);
+    $('.popup').animate({
+      right: +collapseWidth + 40,
+    }, 500);
     $('.toggler').addClass('active');
+    $('.popup').addClass('nav-open');
     if ($(window).width() < 576)
       $('body').css('overflow-y', 'hidden');
   }
